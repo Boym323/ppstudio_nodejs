a0b772449ce02b58a8857482dabec0c7d55a4831
Martin Pomykal <martin@pomykal.cz>
2026-04-27 18:19:55 +0200

    Oprava seznamu povolených vývojových původů v konfiguraci Next.js.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.0.143", "ppstudio.cz", "www.ppstudio.cz"],
+  allowedDevOrigins: ["192.168.0.143", "ppstudio.cz", "www.ppstudio.cz","192.168.0.143"],
 };
 
 export default nextConfig;
